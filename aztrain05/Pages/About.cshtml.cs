bec4049ee0fd00f52a0d51a1a7f7a6fc92969005
David Blizzard <david.blizzard@i2ipophealth.com>
2019-06-17 15:36:50 -0500

    h

--- aztrain05/Pages/About.cshtml.cs
+++ aztrain05/Pages/About.cshtml.cs
@@ -12,7 +12,7 @@ namespace aztrain05.Pages
 
         public void OnGet()
         {
-            Message = "This is test number 2.";
+            Message = "This is test number 3.";
         }
     }
 }
